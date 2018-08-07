b29b5da824708d8ab97632f11250a4a45cba528c
Sagar Shrestha <sshrestha420@gmail.com>
2018-08-07 14:30:52 +0930

    Module 11 complete

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,23 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { ProductListComponet } from './products/product-list.components';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
-import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponet,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponet},
+      {path: 'products/:id', component:ProductDetailComponent},
+      {path: 'welcome',component:WelcomeComponent},
+      {path: '',redirectTo:'welcome', pathMatch: 'full'},
+      {path: '**',redirectTo:'welcome', pathMatch: 'full'}
+    ])
 
   ],
   bootstrap: [AppComponent]
